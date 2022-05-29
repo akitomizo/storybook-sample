96484500c97329ac9a2a444968162fc23fd8608f
akitomizo <ak.mizo.112@gmail.com>
2022-05-29 18:05:46 +0900

    add simple components  with react ver18→17

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,10 @@
 module.exports = {
   "stories": [
+    '../src/components/**/*.stories.js',
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  // "stories": ['../src/components/**/*.stories.js'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
